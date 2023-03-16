d1d0b7b4f2dbdbec1bf9e2ef76044a7a91111794
GauravAChavan <dev.gauravchavan@gmail.com>
2023-03-16 23:37:26 +0530

    added global exception handler for InvalidInputException

--- src/main/java/com/feedapp/exception/handler/FeedExceptionHandler.java
+++ src/main/java/com/feedapp/exception/handler/FeedExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.feedapp.exception.handler;
 
 import com.feedapp.exception.ErrorDTO;
+import com.feedapp.exception.InvalidInputException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +12,16 @@ import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class FeedExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(InvalidInputException.class)
+    public ResponseEntity<Object> handleInvalidInputExceptions(InvalidInputException exception) {
+        ErrorDTO response = new ErrorDTO();
+        response.setDateTime(LocalDateTime.now());
+        response.setMessage(exception.getMessage());
+        ResponseEntity<Object> entity = new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return entity;
+    }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleExceptions(Exception exception) {
